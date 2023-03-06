ea4f734e3928492b0a60585ed71e9d9916205973
Daniele Rizzieri <37268662+danirizziero@users.noreply.github.com>
2023-03-06 16:41:41 +0100

    Update overlay_init.py
    base overlay is now loaded prior the download of the actual bitstream, to solve some ol.is_loaded() faulty situations

--- overlay_init.py
+++ overlay_init.py
@@ -1,9 +1,6 @@
 from pynq import Overlay
 bit_path = "./src/UART_HW_3.bit"
 print(f"[INFO] Loading bitstream {bit_path}...", end="", flush=True)
-ol = Overlay(bit_path, download=False)
-if not ol.is_loaded():
-	ol.download()
-	print("DONE!")
-else:
-	print("already loaded!")
+ol_base = Overlay("base.bit", download=True)
+ol = Overlay(bit_path, download=True)
+print("DONE!")
